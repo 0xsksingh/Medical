932529d8f1780aee09ef001fe5599564f788c019
kamal eth <kamalthedev7@gmail.com>
2023-04-24 11:27:19 +0000

    feat: adding the rainbow connect btn

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useState, useEffect } from "react";
@@ -9,15 +10,49 @@ import Verify from "./pages/verify/Verify.jsx";
 import Home from "./pages/home/Home.jsx";
 import PagenotFound from "./pages/landing/PagenotFound";
 import Assistance from "./pages/home/components/assistance/Assistance";
+// rainbow wallet
+import '@rainbow-me/rainbowkit/styles.css';
+
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import { configureChains, createClient, WagmiConfig } from 'wagmi';
+import { mainnet, polygon, optimism, polygonMumbai } from 'wagmi/chains';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
+
 function App() {
   const [account, setAccount] = useState("");
   const [contract, setContract] = useState("");
-  const [provider, setProvider] = useState("");
-  useEffect(() => {
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
+  const [provider, setProvider] = useState(null);
 
+  const { chains, provider: wagmiProvider } = configureChains(
+    [mainnet, polygon, optimism, polygonMumbai],
+    [
+      alchemyProvider({ apiKey: process.env.ALCHEMY_ID }),
+      publicProvider()
+    ]
+  );
+  
+  const { connectors } = getDefaultWallets({
+    appName: 'My RainbowKit App',
+    projectId: 'YOUR_PROJECT_ID',
+    chains
+  });
+  
+  const wagmiClient = createClient({
+    autoConnect: true,
+    connectors,
+    provider: wagmiProvider
+  })
+
+  
+  useEffect(() => {
     const loadProvider = async () => {
-      if (provider) {
+      if (window.ethereum) {
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+
         window.ethereum.on("chainChanged", () => {
           window.location.reload();
         });
@@ -25,7 +60,7 @@ function App() {
         window.ethereum.on("accountsChanged", () => {
           window.location.reload();
         });
-        await provider.send("eth_requestAccounts", []);
+
         const signer = provider.getSigner();
         const address = await signer.getAddress();
         setAccount(address);
@@ -37,33 +72,37 @@ function App() {
           signer
         );
 
+
         setContract(contract);
         setProvider(provider);
       } else {
         console.error("Metamask is not installed");
       }
     };
-    provider && loadProvider();
+    loadProvider();
   }, []);
+  
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="register" element={<Register contract={contract} />} />
-          <Route path="verify" element={<Verify />} />
-          <Route
-            path="home/*"
-            element={
-              <Home account={account} provider={provider} contract={contract} />
-            }
-          />
-          <Route path="assistance" element={<Assistance />} />
-
-          <Route path='*' element={<PagenotFound />}/>
-        </Routes>
-        {/* <PagenotFound></PagenotFound> */}
-      </BrowserRouter>
+      <WagmiConfig client={wagmiClient}>
+        <RainbowKitProvider chains={chains}>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Landing />} />
+              <Route path="register" element={<Register contract={contract} />} />
+              <Route path="verify" element={<Verify />} />
+              <Route
+                path="home/*"
+                element={
+                  <Home account={account} provider={provider} contract={contract} />
+                }
+              />
+              <Route path="assistance" element={<Assistance />} />
+              <Route path='*' element={<PagenotFound />} />
+            </Routes>
+          </BrowserRouter>
+        </RainbowKitProvider>
+      </WagmiConfig>
     </div>
   );
 }
--- src/pages/landing/Landing.jsx
+++ src/pages/landing/Landing.jsx
@@ -5,133 +5,21 @@ import Header from "./Header";
 import { Button, Typography, Box, Stack, Card, Link } from "@mui/material";
 import {BsFillClipboard2PlusFill} from 'react-icons/bs'
 import {TbAmbulance} from 'react-icons/tb'
-import logo from './images/healTracjk.jpeg'
+import logo from './images/healTracjk.jpg'
 import doc from './images/Sprints1.jpg'
+import { ConnectButton } from "@rainbow-me/rainbowkit";
 function Landing() {
   return (
-    // <div>
-    //   {/* <Header></Header> */}
-    //   {/* <button>
-    //     <a href="/register">Register</a>
-    //   </button> */}
-    //   <Stack
-    //     mt={0}
-    //     // direction="row" spacing={70}
-    //     direction={{ xs: "column", sm: "row" }}
-    //     spacing={{ xs: 1, sm: 2, md: 40 }}
-    //   >
-    //     <img src={logo} alt="image" className='img'/>
-    //   <Header></Header>
-    //   <Box sx={{ w: 30, mt: '4rem', ml:'4rem', display:'inline-block',positon: 'absolute', }}>
-    //     <Stack
-    //     mx={3} my={4}
-    //      direction={{ xs: 'column', sm: 'row' }}
-    //      spacing={{ xs: 1, sm: 2, md: 3 }}
-    //      >
-    //       {/* <Button variant="contained" size='large'
-    //       sx={{ fontSize: '1.2rem', padding:'.6rem 1.2rem' }}
-    //       style={{ backgroundColor: 'rgb(0, 255, 119)' , boxShadow: "3px 3px 3px green"}}
-
-    //       ></Button> */}
-    //         <Button
-    //           variant="contained"
-    //           size="large"
-    //           sx={{ fontSize: "1.2rem", padding: ".6rem 1.2rem" }}
-    //           style={{
-    //             backgroundColor: "rgb(0, 255, 119)",
-    //             boxShadow: "3px 3px 3px green",
-    //           }}
-    //           href='/home'
-    //         >
-    //           Login
-    //         </Button>
-    //         <Button
-    //           variant="contained"
-    //           sx={{ fontSize: "1.2rem", padding: "0 1.2rem" }}
-    //           style={{
-    //             backgroundColor: "rgb(0, 255, 119)",
-    //             boxShadow: "3px 3px 3px green",
-    //           }}
-    //           href="/register"
-    //         >
-    //           Register
-    //         </Button>
-    //       </Stack>
-    //     </Box>
-    //   </Stack>
-    //   <CoverImage></CoverImage>
-    //   <Box
-    //   sx={{
-    //     display:'inline-block',
-    //     marginLeft:'50%'
-    //   }}>
-    //     <Box
-    //     sx={{
-    //       // height:'400px',
-    //       // width:'200px',
-    //     }}>
-    //       {/* <Typography>
-    //       HealTrack helps you manage your medical history seamlessly</Typography>
-    //       <Typography>so you can focus on your health.</Typography>
-    //       <Typography> From storing reports to viewing your timeline, we've got you covered
-    //       </Typography> */}
-    //       <Stack>
-    //       <Typography variant="h3" color={'teal'}>Your health, Our Priority.</Typography>
-    //       <Box
-    //       sx={{
-    //         marginLeft:'60px',
-    //         // boxShadow: "3px 3px 3px green",
-    //       }}>
-    //         <ul>
-    //         {/* <i class="fa-solid fa-triangle">  </i> */}
-    //         <Stack sx={{color:'green'}}
-    //         direction={"row"}>
-    //         <div className="icon"><BsFillClipboard2PlusFill ></BsFillClipboard2PlusFill></div><Typography color={'#bb6868'} variant="h5">Manage your medical history seamlessly</Typography>
-    //         </Stack>
-
-    //         <Stack sx={{color:'green'}}
-    //         direction={"row"}
-    //         >
-    //         <div className="icon"><BsFillClipboard2PlusFill ></BsFillClipboard2PlusFill></div><Typography color={'#bb6868'} variant="h5">Say GoodBye to scattered medical records</Typography>
-    //         </Stack>
-
-    //         <Stack sx={{color:'green'}}
-    //         direction={"row"}
-    //         >
-    //         <div className="icon"><BsFillClipboard2PlusFill ></BsFillClipboard2PlusFill></div><Typography color={'#bb6868'} variant="h5">Where care meets convenience.</Typography>
-    //         </Stack>
-
-    //         <Stack sx={{color:'green'}}
-    //         direction={"row"}
-    //         >
-    //         <div className="icon"><BsFillClipboard2PlusFill ></BsFillClipboard2PlusFill></div><Typography color={'#bb6868'}  variant="h5">Simplify your life, One record at a time.</Typography>
-    //         </Stack>
-
-    //         </ul>
-    //       </Box>
-    //       </Stack>
-
-    //     </Box>
-    //   </Box>
-
-    // </div>
-
     <Box>
       <Card
       sx={{
         display:'flex',
-        // width:'500px',
-        // marginLeft:'800px',
-        // mt:'50px',
-        // justifyContent:'right',
-        // alignItems:'right',
         background:'#9fd1dd',
         marginBottom:'50px',
         position:'sticky',
         zIndex:'1',
         top:'0',
         opacity:'.7',
-
       }}>
         <Stack
         direction={{ xs: 'column', sm: 'row' }}
@@ -179,6 +67,7 @@ function Landing() {
           >
           Contact Us
           </Button>
+          <ConnectButton />
           </Box>
           </Stack>
       </Card>
